fb0db3fa05e4d9514e175a11120aa534862519e2
xien <2383759126@qq.com>
2024-02-14 21:50:43 +0800

    添加多种解决方法

---
+++ 解决星璇机枪/Configuration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace fixbugpe
+{
+    public class Configuration
+    {
+        public static readonly string FilePath = Path.Combine(TShock.SavePath, "解决PE锤子喝药等相关bug.json");
+        // 你可以在这里添加你希望排除的物品信息
+        [JsonProperty("免检测物品列表")]
+        public List<int> ExemptItemList { get; set; } = new List<int> { 205, 206, 207, 1128 };
+
+        [JsonProperty("是否杀死")]
+        public bool KillPlayerOnUse { get; set; } = true;
+
+        [JsonProperty("是否上buff")]
+        public bool ApplyBuffOnUse { get; set; } = false;
+
+        [JsonProperty("上什么buff")]
+        public int[] BuffTypes { get; set; } = { 163, 149 , 23 , 164 };
+
+        [JsonProperty("是否踢出")]
+        public bool KickPlayerOnUse { get; set; } = false;
+
+
+
+
+        public void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.Write(str);
+                }
+            }
+        }
+
+        public static Configuration Read(string path)
+        {
+            if (!File.Exists(path))
+                return new Configuration();
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    var cf = JsonConvert.DeserializeObject<Configuration>(sr.ReadToEnd());
+                    return cf;
+                }
+            }
+        }
+    }
+}
---
+++ 解决星璇机枪/fixbugpe.cs
@@ -0,0 +1,99 @@
+﻿using TerrariaApi.Server;
+using TShockAPI;
+using Terraria;
+using System.Collections.Generic;
+using TShockAPI.Hooks;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+
+namespace fixbugpe
+{
+    [ApiVersion(2, 1)]
+    public class 解决PE锤子喝药等相关bug : TerrariaPlugin
+    {
+        public override string Author => "熙恩，感谢恋恋";
+        public override string Description => "解决PE锤子喝药等相关bug";
+        public override string Name => "解决PE锤子喝药等相关bug";
+        public override Version Version => new Version(1, 0, 2);
+        public static Configuration Config;
+        public bool otherPluginExists = false;
+
+        public 解决PE锤子喝药等相关bug(Main game) : base(game)
+        {
+            LoadConfig();
+        }
+
+        private static void LoadConfig()
+        {
+            Config = Configuration.Read(Configuration.FilePath);
+            Config.Write(Configuration.FilePath);
+        }
+
+        private static void ReloadConfig(ReloadEventArgs args)
+        {
+            LoadConfig();
+            args.Player?.SendSuccessMessage("[{0}] 重新加载配置完毕。", typeof(解决PE锤子喝药等相关bug).Name);
+        }
+
+        public override void Initialize()
+        {
+            // 事件订阅代码
+            GeneralHooks.ReloadEvent += ReloadConfig;
+            ServerApi.Plugins.Get<Chireiden.TShock.Omni.Plugin>().Detections.SwapWhileUse += OnSwapWhileUse;
+        }
+
+        private void OnSwapWhileUse(int playerId, int slot)
+        {
+            TSPlayer player = TShock.Players[playerId];
+
+            if (player != null && !player.HasPermission("fixbugpe"))
+            {
+                // 获取玩家正在使用的物品
+                Item item = player.TPlayer.inventory[slot];
+
+                // 检查是否是排除列表中的任意一个物品
+                if (item != null && Config.ExemptItemList.Contains(item.type))
+                {
+                    // 玩家持有排除的物品，不执行踢出逻辑
+                    return;
+                }
+
+                if (Config.KickPlayerOnUse)
+                {
+                    TShock.Utils.Broadcast("玩家 " + player.Name + " 因为卡换格子bug被踢出", Color.Green);
+                    player.Kick("因为卡换格子bug被踢出");
+                }
+
+                if (Config.KillPlayerOnUse)
+                {
+                    TShock.Utils.Broadcast("玩家 " + player.Name + " 因为卡换格子bug被杀死", Color.Green);
+                    player.Dead = true;
+                }
+
+                if (Config.ApplyBuffOnUse)
+                {
+                    TShock.Utils.Broadcast("玩家 " + player.Name + " 因为卡换格子bug被上buff", Color.Green);
+                    foreach (int buffType in Config.BuffTypes)
+                    {
+                        player.SetBuff(buffType, 3600);
+                    }
+                }
+
+
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // 移除事件订阅
+                ServerApi.Plugins.Get<Chireiden.TShock.Omni.Plugin>().Detections.SwapWhileUse -= OnSwapWhileUse;
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
+
+
